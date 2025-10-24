a6afd2ae8adc65e5798c7a1681eaa81ee975c772
Tangent Chang <andy23512@gmail.com>
2025-10-24 16:27:38 +0800

    refactor: move pipe folder into app

--- src/app/component/toolbar.ts
+++ src/app/component/toolbar.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { IconGuardPipe } from '../../pipe/icon-guard.pipe';
+import { IconGuardPipe } from '../pipe/icon-guard.pipe';
 
 @Component({
   imports: [MatListModule, MatIconModule, MatTooltipModule, IconGuardPipe],
