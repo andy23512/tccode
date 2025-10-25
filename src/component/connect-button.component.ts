57b74a91dd8c3b060cc046c34ddabc8e37482dfe
Tangent Chang <andy23512@gmail.com>
2025-10-25 15:30:39 +0800

    feat: add progress spinner for loading chords

--- src/component/connect-button.component.ts
+++ src/component/connect-button.component.ts
@@ -1,12 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { IconGuardPipe } from '../pipe/icon-guard.pipe';
 import { DeviceStore } from '../store/device.store';
 
 @Component({
-  imports: [MatButtonModule, MatIconModule, MatTooltipModule, IconGuardPipe],
+  imports: [
+    IconGuardPipe,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+  ],
   selector: 'app-connect-button',
   templateUrl: './connect-button.component.html',
 })
@@ -15,6 +22,17 @@ export class ConnectButtonComponent {
   public deviceConnected = this.deviceStore.isConnected;
 
   public isWebSerialApiSupported = 'serial' in navigator;
+
+  public isChordLoaded = computed<boolean>(() => {
+    return !!this.deviceStore.chordLibraryLoadStatus()?.complete;
+  });
+  public chordLoadProgress = computed<number>(() => {
+    const loadStatus = this.deviceStore.chordLibraryLoadStatus();
+    if (!loadStatus) {
+      return 0;
+    }
+    return (loadStatus.loaded / loadStatus.total) * 100;
+  });
 
   public async connect() {
     await this.deviceStore.connect();
