d996743319d245ec1243fbe617ea3af66e6f1407
Tangent Chang <andy23512@gmail.com>
2025-10-26 15:29:11 +0800

    feat: add show welcome dialog setting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ChordEditorComponent } from '../component/chord-editor.component';
 import { ToolbarComponent } from '../component/toolbar.component';
+import { SettingStore } from '../store/setting.store';
 
 @Component({
   imports: [ChordEditorComponent, ToolbarComponent],
@@ -10,4 +11,14 @@ import { ToolbarComponent } from '../component/toolbar.component';
     class: 'h-screen flex',
   },
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  public settingStore = inject(SettingStore);
+
+  public ngOnInit(): void {
+    const showWelcomeDialogWhenStart =
+      this.settingStore.showWelcomeDialogWhenStart();
+    if (showWelcomeDialogWhenStart) {
+      console.log('show');
+    }
+  }
+}
---
+++ src/store/setting.store.ts
@@ -0,0 +1,30 @@
+import {
+  withDevtools,
+  withStorageSync,
+} from '@angular-architects/ngrx-toolkit';
+import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import { Setting } from '../model/setting.model';
+
+const INITIAL_SETTING: Setting = {
+  showWelcomeDialogWhenStart: true,
+};
+
+export const SettingStore = signalStore(
+  { providedIn: 'root' },
+  withDevtools('setting'),
+  withStorageSync({
+    key: 'setting',
+    parse(stateString: string) {
+      return { ...INITIAL_SETTING, ...JSON.parse(stateString) };
+    },
+  }),
+  withState(INITIAL_SETTING),
+  withMethods((store) => ({
+    set<K extends keyof Setting>(key: K, value: Setting[K]) {
+      patchState(store, (state) => ({
+        ...state,
+        [key]: value,
+      }));
+    },
+  })),
+);
