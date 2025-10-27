203021836ca7d4bbf8adbe2241f1f47d88e994af
Tangent Chang <andy23512@gmail.com>
2025-10-27 19:48:52 +0800

    chore: move g4 file and add another sample chord library

--- src/data/sample-chords.const.ts
+++ src/data/sample-chords.const.ts
@@ -1,7 +1,37 @@
-export const SAMPLE_CHORDS = `
-b + c         = because
-y + o + u     = you
-b + a + k     = back
+export const SAMPLE_CHORDS_1 = `
+b + c = because
+y + o + u = you
+b + a + k = back
 d + o + w + n = down
-i + m + p     = important
+i + m + p = important
+`.trim();
+
+export const SAMPLE_CHORDS_2 = `
+[ + ] = - [ ]
+a + n + o = annotation
+b + n = backend
+b + l = boolean
+c + s = class
+c + o = component
+c + o + / = Component
+c + n + . = console.log
+c + o + n = container
+d + s = describe
+d + v = directive
+d + v + / = Directive
+d + j = django
+d + k = docker
+f + n = frontend
+i + m = import
+i + n = interface
+o + b + s = Observable
+p + n = preference
+r + o = readonly
+r + m = remove
+s + v = service
+s + v + / = Service
+s + t = string
+s + u + b = subscribe
+u + p = update
+v + i = viewer
 `.trim();
