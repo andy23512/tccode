ad06824c2d1448b856088b14ef807aa106cb2f77
Tangent Chang <andy23512@gmail.com>
2025-10-28 11:05:23 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/andy23512/tccode/commit/217fbd9f28ae050963a0764ed318cc95bc2a9d94

--- chunk-H64LKTC5.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c}from"./chunk-UN5VW5ND.js";import"./chunk-ONYQWDOR.js";import"./chunk-QEAS3NOD.js";c();export{a as conf,b as language};
---
+++ chunk-RGMOOUXT.js
@@ -0,0 +1,1 @@
+import{a,b,c}from"./chunk-D3OGLK2X.js";import"./chunk-YTVRDYRF.js";import"./chunk-XZAWOCBM.js";c();export{a as conf,b as language};
