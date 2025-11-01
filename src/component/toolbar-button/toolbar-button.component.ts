946bec9c157093a8f2946bda338dc8436bd0494b
Tangent Chang <andy23512@gmail.com>
2025-11-01 11:33:12 +0800

    fix: fix menu position right error

--- src/component/toolbar-button/toolbar-button.component.ts
+++ src/component/toolbar-button/toolbar-button.component.ts
@@ -1,13 +1,25 @@
-import { Component, EventEmitter, input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  input,
+  Output,
+  viewChild,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatMenu } from '@angular/material/menu';
+import { MatMenu, MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { Icon } from '../../model/icon.model';
 import { IconGuardPipe } from '../../pipe/icon-guard.pipe';
 
 @Component({
-  imports: [MatTooltipModule, MatButtonModule, MatIconModule, IconGuardPipe],
+  imports: [
+    IconGuardPipe,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTooltipModule,
+  ],
   selector: 'app-toolbar-button',
   templateUrl: 'toolbar-button.component.html',
 })
@@ -17,4 +29,14 @@ export class ToolbarButtonComponent {
   public disabled = input<boolean>(false);
   public menu = input<MatMenu | null>(null);
   @Output() public buttonClick = new EventEmitter();
+  public menuTrigger = viewChild<MatMenuTrigger>('menuTrigger');
+
+  public onButtonClick() {
+    this.buttonClick.emit();
+    const menu = this.menu();
+    const menuTrigger = this.menuTrigger();
+    if (menu && menuTrigger) {
+      menuTrigger.openMenu();
+    }
+  }
 }
