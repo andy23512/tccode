7efa523a069912c14cc256766fdd4c6bf42cc2c5
Tangent Chang <andy23512@gmail.com>
2025-11-21 08:37:12 +0800

    feat: disable save to device when there is any editor error

--- src/component/save-button/save-button.component.ts
+++ src/component/save-button/save-button.component.ts
@@ -1,7 +1,16 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltip } from '@angular/material/tooltip';
+import * as monaco from 'monaco-editor';
 import { IconGuardPipe } from '../../pipe/icon-guard.pipe';
 import { ChordSaverService } from '../../service/chord-saver.service';
 import { SaveToDeviceDialogComponent } from '../save-to-device-dialog/save-to-device-dialog.component';
@@ -15,12 +24,32 @@ import { ToolbarButtonComponent } from '../toolbar-button/toolbar-button.compone
     IconGuardPipe,
     MatIconModule,
     MatMenuModule,
+    MatTooltip,
     ToolbarButtonComponent,
   ],
 })
-export class SaveButtonComponent {
+export class SaveButtonComponent implements OnInit, OnDestroy {
   private chordSaverService = inject(ChordSaverService);
   private matDialog = inject(MatDialog);
+  private disposable: monaco.IDisposable | null = null;
+  public hasEditorError = signal<boolean | null>(null);
+
+  public ngOnInit(): void {
+    this.disposable = monaco.editor.onDidChangeMarkers(() => {
+      const model = monaco.editor.getModels()[0];
+      const allMarkers = monaco.editor.getModelMarkers({
+        resource: model.uri,
+      });
+      const errors = allMarkers.filter(
+        (marker) => marker.severity === monaco.MarkerSeverity.Error,
+      );
+      this.hasEditorError.set(errors.length > 0);
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.disposable?.dispose();
+  }
 
   public openSaveToDeviceDialog() {
     this.matDialog.open(SaveToDeviceDialogComponent, {
--- src/component/save-to-device-dialog/save-to-device-dialog.component.ts
+++ src/component/save-to-device-dialog/save-to-device-dialog.component.ts
@@ -1,13 +1,7 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-  signal,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatStepperModule } from '@angular/material/stepper';
-import * as monaco from 'monaco-editor';
 
 @Component({
   selector: 'app-save-to-device-dialog',
@@ -15,15 +9,4 @@ import * as monaco from 'monaco-editor';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [MatDialogModule, MatButtonModule, MatStepperModule],
 })
-export class SaveToDeviceDialogComponent implements OnInit {
-  public noEditorError = signal<boolean | null>(null);
-
-  public ngOnInit(): void {
-    const model = monaco.editor.getModels()[0];
-    const allMarkers = monaco.editor.getModelMarkers({ resource: model.uri });
-    const errors = allMarkers.filter(
-      (marker) => marker.severity === monaco.MarkerSeverity.Error,
-    );
-    this.noEditorError.set(errors.length === 0);
-  }
-}
+export class SaveToDeviceDialogComponent {}
